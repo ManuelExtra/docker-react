28627838ca7ed0e40d4029aada4293e0ced59032
manuelExtra <olaleyeemmanuel23@gmail.com>
2023-11-17 14:19:21 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hi there... This is Emmanuel.  Changed
+          <br/>
+          Docker is a nice tool for backend developers.
+          It can be used with different CI/CD providers
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
